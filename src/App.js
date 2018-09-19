b28d868e22e166761520965f0e43cd04ab513440
Dylan Schroeder <schrodac@gmail.com>
2018-09-19 14:28:16 -0500

    can go from landing to dashboard/minor style changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <nav>
+              <ul>
+                <li><Link to="/">Landing</Link></li>
+                <li><Link to="/dashboard">Dashboard</Link></li>
+              </ul>
+            </nav>
+          </header>
+          <main>
+            <Route exact path="/"
+              component={() => <h1>Description goes here</h1>} />
+
+            {/* <Route exact path="/dashboard"
+              component={Dashboard} /> */}
+          </main>
+        </div>
+      </BrowserRouter>
     );
   }
 }
