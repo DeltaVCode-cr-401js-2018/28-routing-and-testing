5fa23d449b26c48f703e458078540f1367f482f4
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-09-20 13:47:23 -0500

    set up App state on dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
 
 export default class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state= {
+      stores:[],
+      error:null,
+    }
+  }
+
+  componentDidMount(){
+    console.log('__State__',this.state);
+  }
+
+  componentDidUpdate(){
+    console.log('__State__',this.state);
+  }
+
   render() {
     return (
       <React.Fragment>
