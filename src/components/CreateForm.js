f0907ff9ef106bd98883e474f30c0ee1f473223a
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-09-20 13:49:00 -0500

    Form created. Needs handleSubmit

--- src/components/CreateForm.js
+++ src/components/CreateForm.js
@@ -1,1 +1,43 @@
 import React, { Component } from 'react';
+
+const defaultState = {
+  storeName: '',
+  min: 0,
+  max: 1,
+  avg: 0,
+}
+
+export default class Form extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = defaultState;
+  }
+
+  onComplete = (event) => {
+    event.preventDefault();
+
+    console.log(this.state);
+    this.props.handleSubmit(this.state);
+
+    this.setState(defaultState);
+  } 
+
+  handleChange = (event) => {
+    const {name, value, type} = event.target;
+    
+    this.setState({[name]: type === 'number' ? +value : value});
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onComplete}>
+        <input type="text" name="storeName" placeholder="New store name" onChange={this.handleChange}/>
+        <input type="number" name="max" placeholder="min Number of customers" onChange={this.handleChange}/>
+        <input type="number" name="min" placeholder="max Number of customers" onChange={this.handleChange}/>
+        <input type="number" name="avg" placeholder="avg sales" onChange={this.handleChange}/>
+        <input type="submit"/>
+      </form>
+    );
+  }
+}
