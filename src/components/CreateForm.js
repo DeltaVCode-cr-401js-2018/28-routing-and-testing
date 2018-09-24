5e90505657f354b488f775c4e4abf3aa00ed5af2
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-09-24 14:21:07 -0500

    added basic floats

--- src/components/CreateForm.js
+++ src/components/CreateForm.js
@@ -31,13 +31,16 @@ export default class Form extends Component {
 
   render() {
     return (
-      <form onSubmit={this.onComplete}>
-        <label>Store Name<input type="text" name="storeName" value={this.state.storeName} onChange={this.handleChange}/></label>
-        <label>Max # of Customers/hr<input type="number" step="1" name="max" value={this.state.max} onChange={this.handleChange}/></label>
-        <label>Min # of Customers/hr<input type="number" step="1" name="min" value={this.state.min} onChange={this.handleChange}/></label>
-        <label>Avg # of sales/customers<input type="number" step="0.01" name="avg" value={this.state.avg} onChange={this.handleChange}/></label>
-        <input type="submit"/>
-      </form>
+      <div className="form">
+        <h2>Add Store</h2>
+        <form onSubmit={this.onComplete}>
+          <label>Store Name<input type="text" name="storeName" value={this.state.storeName} onChange={this.handleChange}/></label>
+          <label>Max # of Customers/hr<input type="number" step="1" name="max" value={this.state.max} onChange={this.handleChange}/></label>
+          <label>Min # of Customers/hr<input type="number" step="1" name="min" value={this.state.min} onChange={this.handleChange}/></label>
+          <label>Avg # of sales/customers<input type="number" step="0.01" name="avg" value={this.state.avg} onChange={this.handleChange}/></label>
+          <input type="submit"/>
+        </form>
+      </div>
     );
   }
 }
